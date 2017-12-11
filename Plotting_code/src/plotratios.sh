f5908561dd3093177f2c5a06a811cced07b6bb52
RosalynLP <33020850+RosalynLP@users.noreply.github.com>
2017-12-11 11:35:40 +0000

    Update plotratios.sh

--- Plotting_code/src/plotratios.sh
+++ Plotting_code/src/plotratios.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-python plotratio.py nCTEQ15_56_26 cteq6 1
-python plotratio.py nCTEQ15_56_26 cteq6 2
-python plotratio.py nCTEQ15FullNuc_56_26 cteq6 3
-python plotratio.py nCTEQ15FullNuc_56_26 cteq6 -1
-python plotratio.py nCTEQ15FullNuc_56_26 cteq6 -2
-python plotratio.py nCTEQ15FullNuc_56_26 cteq6 21
-python plotratio.py nCTEQ15_208_82 cteq6 1
-python plotratio.py nCTEQ15_208_82 cteq6 2
-python plotratio.py nCTEQ15FullNuc_208_82 cteq6 3
-python plotratio.py nCTEQ15FullNuc_208_82 cteq6 -1
-python plotratio.py nCTEQ15FullNuc_208_82 cteq6 -2
-python plotratio.py nCTEQ15FullNuc_208_82 cteq6 21
 python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 1
 python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 2
 python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 3
@@ -26,15 +14,15 @@ python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo -2
 python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo 21
 python plotratio.py nCTEQ15_56_26 nCTEQ15_1 1
 python plotratio.py nCTEQ15_56_26 nCTEQ15_1 2
+python plotratio.py nCTEQ15_56_26 nCTEQ15_1 -1
+python plotratio.py nCTEQ15_56_26 nCTEQ15_1 -2
 python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 3
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 -1
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 -2
 python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 21
 python plotratio.py nCTEQ15_208_82 nCTEQ15_1 1
 python plotratio.py nCTEQ15_208_82 nCTEQ15_1 2
+python plotratio.py nCTEQ15_208_82 nCTEQ15_1 -1
+python plotratio.py nCTEQ15_208_82 nCTEQ15_1 -2
 python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 3
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 -1
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 -2
 python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 21
 
 
