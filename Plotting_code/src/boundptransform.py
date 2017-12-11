4126a53c4b6e16ecf174be89727d6fb2a2b56f7b
RosalynLP <33020850+RosalynLP@users.noreply.github.com>
2017-12-11 11:35:12 +0000

    Update boundptransform.py

--- Plotting_code/src/boundptransform.py
+++ Plotting_code/src/boundptransform.py
@@ -23,14 +23,20 @@ def numericalSort(value):
 # Getting data
 x,fu,fuerr=np.loadtxt('/home/rosalyn/nuclear/PDFplots/{0}/data_2_{0}.txt'.format(fullnucpdf), unpack=True)
 x0,fd,fderr=np.loadtxt('/home/rosalyn/nuclear/PDFplots/{0}/data_1_{0}.txt'.format(fullnucpdf), unpack=True)
+x00,fub,fuberr=np.loadtxt('/home/rosalyn/nuclear/PDFplots/{0}/data_-2_{0}.txt'.format(fullnucpdf), unpack=True)
+x000,fdb,fdberr=np.loadtxt('/home/rosalyn/nuclear/PDFplots/{0}/data_-1_{0}.txt'.format(fullnucpdf), unpack=True)
 
 # Transforming from full nuclear to bound proton
 fubp = ((A-Z)*fd-Z*fu)/(A-2*Z)
 fdbp = ((A-Z)*fu-Z*fd)/(A-2*Z)
+fubbp = ((A-Z)*fdb-Z*fub)/(A-2*Z)
+fdbbp = ((A-Z)*fub-Z*fdb)/(A-2*Z)
 
 # Calculating errors
 fubperr = np.sqrt((((A-Z)/(A-2*Z))*(fderr))**2 + ((Z/(A-2*Z))*(fuerr))**2)
 fdbperr = np.sqrt((((A-Z)/(A-2*Z))*(fuerr))**2 + ((Z/(A-2*Z))*(fderr))**2)
+fubbperr = np.sqrt((((A-Z)/(A-2*Z))*(fdberr))**2 + ((Z/(A-2*Z))*(fuberr))**2)
+fdbbperr = np.sqrt((((A-Z)/(A-2*Z))*(fuberr))**2 + ((Z/(A-2*Z))*(fdberr))**2)
 
 # Printing data to files
 with open('/home/rosalyn/nuclear/PDFplots/{0}/data_2_{0}.txt'.format(boundppdf), 'w') as output:
@@ -39,6 +45,12 @@ with open('/home/rosalyn/nuclear/PDFplots/{0}/data_2_{0}.txt'.format(boundppdf),
 with open('/home/rosalyn/nuclear/PDFplots/{0}/data_1_{0}.txt'.format(boundppdf), 'w') as output:
     for index in range(len(x)):
          	output.write(str(x[index]) + "\t" + str(fdbp[index]) + "\t" + str(fdbperr[index]) + "\n")
+with open('/home/rosalyn/nuclear/PDFplots/{0}/data_-2_{0}.txt'.format(boundppdf), 'w') as output:
+    for index in range(len(x)):
+         	output.write(str(x[index]) + "\t" + str(fubbp[index]) + "\t" + str(fubbperr[index]) + "\n")
+with open('/home/rosalyn/nuclear/PDFplots/{0}/data_-1_{0}.txt'.format(boundppdf), 'w') as output:
+    for index in range(len(x)):
+         	output.write(str(x[index]) + "\t" + str(fdbbp[index]) + "\t" + str(fdbbperr[index]) + "\n")
 
 # Now to plot pdfs with errors
 fig=plt.figure()
@@ -59,6 +71,30 @@ ax.set_xscale('log')
 ax.plot(x,fdbp,color="r")
 ax.fill_between(x, fdbp+np.absolute(fdbperr), fdbp-np.absolute(fdbperr),alpha=0.5,facecolor="r")
 plt.title("{0} bound proton PDF, PID = 1".format(boundppdf))
+plt.xlabel("x")
+plt.ylabel("xf")
+#plt.ylim(-2,10)
+plt.xlim(1e-3,1)
+plt.savefig('/home/rosalyn/nuclear/PDFplots/{0}/{0}_d_plot'.format(boundppdf))
+
+fig=plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xscale('log')
+ax.plot(x,fubbp,color="r")
+ax.fill_between(x, fubbp+np.absolute(fubbperr), fubbp-np.absolute(fubbperr),alpha=0.5,facecolor="r")
+plt.title("{0} bound proton PDF, PID = -2".format(boundppdf))
+plt.xlabel("x")
+plt.ylabel("xf")
+#plt.ylim(-2,10)
+plt.xlim(1e-3,1)
+plt.savefig('/home/rosalyn/nuclear/PDFplots/{0}/{0}_ubar_plot'.format(boundppdf))
+
+fig=plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xscale('log')
+ax.plot(x,fdbbp,color="r")
+ax.fill_between(x, fdbbp+np.absolute(fdbbperr), fdbbp-np.absolute(fdbbperr),alpha=0.5,facecolor="r")
+plt.title("{0} bound proton PDF, PID = -1".format(boundppdf))
 plt.xlabel("x")
 plt.ylabel("xf")
 #plt.ylim(-2,10)
