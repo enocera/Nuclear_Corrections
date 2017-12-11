1cef940314ac05a8727d815df133a9346f330b5f
Emanuele Nocera <enocera@t014.ph.ed.ac.uk>
2017-12-11 12:41:28 +0000

    Optimised bash scripts

--- Plotting_code/src/generatepdfs.sh
+++ Plotting_code/src/generatepdfs.sh
@@ -1,53 +1,34 @@
 #!/bin/bash
 
-python2.7 genpdf.py CT14nlo 21 3.1622
-python2.7 genpdf.py CT14nlo 1 3.1622
-python2.7 genpdf.py CT14nlo 2 3.1622
-python2.7 genpdf.py CT14nlo 3 3.1622
-python2.7 genpdf.py CT14nlo -1 3.1622
-python2.7 genpdf.py CT14nlo -2 3.1622
-python2.7 genpdf.py CT14nlo -3 3.1622
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 21 10
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 1 10
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 2 10
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 3 10
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 -1 10
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 -2 10
-python2.7 genpdf.py nCTEQ15FullNuc_56_26 -3 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 21 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 1 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 2 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 3 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 -1 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 -2 10
-python2.7 genpdf.py nCTEQ15FullNuc_208_82 -3 10
-python2.7 genpdf.py cteq6 21 10
-python2.7 genpdf.py cteq6 1 10
-python2.7 genpdf.py cteq6 2 10
-python2.7 genpdf.py cteq6 3 10
-python2.7 genpdf.py cteq6 -1 10
-python2.7 genpdf.py cteq6 -2 10
-python2.7 genpdf.py cteq6 -3 10
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 21 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 1 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 2 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 3 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 -1 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 -2 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Fe56 -3 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 21 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 1 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 2 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 3 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 -1 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 -2 3.1622
-python2.7 genpdf.py EPPS16nlo_CT14nlo_Pb208 -3 3.1622
-python2.7 genpdf.py nCTEQ15_1 21 10
-python2.7 genpdf.py nCTEQ15_1 1 10
-python2.7 genpdf.py nCTEQ15_1 2 10
-python2.7 genpdf.py nCTEQ15_1 3 10
-python2.7 genpdf.py nCTEQ15_1 -1 10
-python2.7 genpdf.py nCTEQ15_1 -2 10
+namePDFset[1]="CT14nlo"
+namePDFset[2]="nCTEQ15FullNuc_56_26"
+namePDFset[3]="nCTEQ15FullNuc_208_82"
+namePDFset[4]="EPPS16nlo_CT14nlo_Fe56"
+namePDFset[5]="EPPS16nlo_CT14nlo_Pb208"
+namePDFset[6]="cteq6"
+namePDFset[7]="nCTEQ15_1"
+
+Q[1]=10     #GeV
+Q[2]=3.1622 #GeV
+
+PID[1]=21
+PID[2]=-3
+PID[3]=-1
+PID[4]=-2
+PID[5]=+2
+PID[6]=+1
+PID[7]=+3
 
 
+for ipdf in `seq 1 7`
+do
+for iQ in `seq 1 2`
+do
+for iPID in `seq 1 7`
+do
+python genpdf.py "${namePDFset[ipdf]}" ${PID[iPID]} ${Q[iQ]}
+done
+done
+done
 
+exit 0
--- Plotting_code/src/plotratios.sh
+++ Plotting_code/src/plotratios.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 1
-python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 2
-python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 3
-python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo -1
-python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo -2
-python plotratio.py EPPS16nlo_CT14nlo_Fe56 CT14nlo 21
-python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo 1
-python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo 2
-python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo 3
-python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo -1
-python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo -2
-python plotratio.py EPPS16nlo_CT14nlo_Pb208 CT14nlo 21
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 1
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 2
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 -1
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 -2
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 3
-python plotratio.py nCTEQ15FullNuc_56_26 nCTEQ15_1 21
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 1
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 2
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 -1
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 -2
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 3
-python plotratio.py nCTEQ15FullNuc_208_82 nCTEQ15_1 21
+namePDFset[1]="nCTEQ15FullNuc_56_26"
+namePDFset[2]="nCTEQ15FullNuc_208_82"
+namePDFset[3]="EPPS16nlo_CT14nlo_Fe56"
+namePDFset[4]="EPPS16nlo_CT14nlo_Pb208"
+
+PDFset[1]="nCTEQ15_1"
+PDFset[2]="nCTEQ15_1"
+PDFset[3]="CT14nlo"
+PDFset[4]="CT14nlo"
+
+PID[1]=21
+PID[2]=-3
+PID[3]=-1
+PID[4]=-2
+PID[5]=+2
+PID[6]=+1
+PID[7]=+3
+
+for ipdf in `seq 1 4`
+do
+for iPID in `seq 1 7`
+do
+python plotratio.py "${namePDFset[ipdf]}" "${PDFset[ipdf]}" ${PID[iPID]}
+done
+done
+
+exit 0
+
 
 
