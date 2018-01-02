681cf0b2673de33dffb8a056b480b8151583e983
Rosalyn <rosalynlaurap@gmail.com>
2018-01-02 13:02:21 +0000

    Also produces plots for bound proton nPDFs with the correct Hessian errors

--- Plotting_code/src/generatepdfs.sh
+++ Plotting_code/src/generatepdfs.sh
@@ -13,28 +13,43 @@ namePDFset[10]="MSTW2008nlo68cl"
 namePDFset[11]="DSSZ_NLO_Fe56"
 namePDFset[12]="DSSZ_NLO_Pb208"
 
+# Fill in the corresponding proton and mass numbers for each PDF set
+A[1]=1
+A[2]=56
+A[3]=208
+A[4]=56
+A[5]=208
+A[6]=1
+A[7]=1
+A[8]=56
+A[9]=208
+A[10]= 1
+A[11]=56
+A[12]=208
+
+Z[1]=1
+Z[2]=26
+Z[3]=82
+Z[4]=26
+Z[5]=82
+Z[6]=1
+Z[7]=1
+Z[8]=26
+Z[9]=82
+Z[10]=1
+Z[11]=26
+Z[12]=208
+
 #This is the energy scale
 Q[1]=10     #GeV
 Q[2]=3.1622 #GeV
 Q[3]=2 #GeV
 
-PID[1]=21
-PID[2]=-3
-PID[3]=-1
-PID[4]=-2
-PID[5]=+2
-PID[6]=+1
-PID[7]=+3
-
-
 for ipdf in `seq 1 12`
 do
 for iQ in `seq 1 3`
 do
-for iPID in `seq 1 7`
-do
-python genpdf.py "${namePDFset[ipdf]}" ${PID[iPID]} ${Q[iQ]}
-done
+python genpdf.py "${namePDFset[ipdf]}" ${A[ipdf]} ${Z[ipdf]} ${Q[iQ]}
 done
 done
 
