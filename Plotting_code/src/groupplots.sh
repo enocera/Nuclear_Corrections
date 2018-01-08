5f31605cba3f78b991181cb47dce3796b3df6243
RosalynLP <rosalynlaurap@gmail.com>
2018-01-08 10:56:10 +0000

    Removed unnecessary plots

--- Plotting_code/src/groupplots.sh
+++ Plotting_code/src/groupplots.sh
@@ -4,20 +4,18 @@ PDFsets[1]="nCTEQ15FullNuc_208_82 EPPS16nlo_CT14nlo_Pb208 DSSZ_NLO_Pb208"
 
 PID[1]=21
 PID[2]=-3
-PID[3]=-1
-PID[4]=-2
-PID[5]=+3
-PID[6]=+11
-PID[7]=-11
-PID[8]=+22
-PID[9]=-22
+PID[3]=+3
+PID[4]=+11
+PID[5]=-11
+PID[6]=+22
+PID[7]=-22
 
 #This is the energy scale
 Q[1]=10     #GeV
 Q[2]=3.1622 #GeV
 Q[3]=2 #GeV
 
-for iPID in `seq 1 9`
+for iPID in `seq 1 7`
 do
 for iQ in `seq 1 3`
 do
