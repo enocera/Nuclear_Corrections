56a13e4d39263dec625e1a8b0837aa4e339b4937
Rosalyn <rosalynlaurap@gmail.com>
2018-01-21 12:36:49 +0000

    Update

--- Plotting_code/src/generatepdfs.sh
+++ Plotting_code/src/generatepdfs.sh
@@ -118,13 +118,13 @@ Q[1]=10     #GeV
 Q[2]=3.1622 #GeV
 Q[3]=2 #GeV
 
-for ipdf in `seq 1 12`
-do
-for iQ in `seq 1 3`
-do
-python genpdf.py "${namePDFset[ipdf]}" ${A[ipdf]} ${Z[ipdf]} ${Q[iQ]}
-done
-done
+#for ipdf in `seq 1 12`
+#do
+#for iQ in `seq 1 3`
+#do
+#python genpdf.py "${namePDFset[ipdf]}" ${A[ipdf]} ${Z[ipdf]} ${Q[iQ]}
+#done
+#done
 
 for ipdf in `seq 31 36`
 do
