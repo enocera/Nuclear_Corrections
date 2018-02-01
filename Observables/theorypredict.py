d409c9efa33ffac3c649c6d8a6aabd78d340616d
RosalynLP <rosalynlaurap@gmail.com>
2018-02-01 16:39:06 +0000

    Further work on python observables code

--- Observables/theorypredict.py
+++ Observables/theorypredict.py
@@ -18,31 +18,38 @@ def list3D(a, b, c):
     lst = [[ ['#' for col in range(c)] for col in range(b)] for row in range(a)]
     return lst
 
-nexp = 2
-nset = 2
+nexp     = 2
+nset     = 2
 maxpoint = 1000
-npt = list2D(nexp,nset)
-obs = list2D(nexp,nset)
-x = list3D(nexp,nset,maxpoint)
-Q2 = list3D(nexp,nset,maxpoint)
-y = list3D(nexp,nset,maxpoint)
-exobs = list3D(nexp,nset,maxpoint)
-Q = list3D(nexp,nset,maxpoint)
+
+npt      = list2D(nexp,nset)
+obs      = list2D(nexp,nset)
+x        = list3D(maxpoint,nexp,nset)
+Q2       = list3D(maxpoint,nexp,nset)
+y        = list3D(maxpoint,nexp,nset)
+exobs    = list3D(maxpoint,nexp,nset)
+Q        = list3D(maxpoint,nexp,nset)
 
 # Initialise data files to be read
-exp = ["CHORUS", "NTV"]
-expset = [["NU", "NB"], ["NUDMN", "NBDMN"]]
+exp      = ["CHORUS", "NTV"]
+expset   = [["NU", "NB"], ["NUDMN", "NBDMN"]]
 
 
-# Read in PDF set name from command line
-pdfset=input("Please enter the PDF set name: ")
+# Read in PDF set name from command line and initialise PDF file
+pdfset   =  input("Please enter the PDF set name: ")
 print("PDF set: " + pdfset)
+
+pdfs     =  lhapdf.mkPDFs(pdfset)
+nrep     =  len(pdfs)
 
 # Reading data
 for iexp in range(0,nexp):
     for iset in range(0,nset):
+
         file = "./commondata/DATA_{0}{1}.dat".format(exp[iexp],expset[iexp][iset])
+        print("-----------------------------------------------------------------")
         print("File being read is " + str(file))
+        print("-----------------------------------------------------------------")
         with open(file) as contents:
             contents = contents.read()
 
@@ -50,14 +57,16 @@ for iexp in range(0,nexp):
         print("Number of data points: " + str(npt[iexp][iset]))
 
         obs[iexp][iset] = str(getelement(contents,1,1))
-        print("Observable: " + obs[iexp][iset])  
-  
+        print("Observable: " + obs[iexp][iset])
+
         for ipt in range(1,int(npt[iexp][iset])+1):
-            x[iexp][iset][ipt] = getelement(contents,2,ipt)
-            Q2[iexp][iset][ipt] = getelement(contents,3,ipt)
-            y[iexp][iset][ipt] = getelement(contents,4,ipt)
-            exobs[iexp][iset][ipt] = getelement(contents,5,ipt)      
-            Q[iexp][iset][ipt] = str(sqrt(float(Q2[iexp][iset][ipt])))
+
+            x[ipt][iexp][iset]     = getelement(contents,2,ipt)
+            Q2[ipt][iexp][iset]    = getelement(contents,3,ipt)
+            y[ipt][iexp][iset]     = getelement(contents,4,ipt)
+            exobs[ipt][iexp][iset] = getelement(contents,5,ipt)
+
+            Q[ipt][iexp][iset]     = str(sqrt(float(Q2[ipt][iexp][iset])))
 
 # Initialise theory details
 ptord        = 1
@@ -72,10 +81,10 @@ mtpole       = 172.5e0
 nflmax       = 5
 Q0           = 1.65e0
 
-apfel.SetPDFSet(pdfset); 
-apfel.SetPerturbativeOrder(ptord);   
+apfel.SetPDFSet(pdfset);
+apfel.SetPerturbativeOrder(ptord);
 apfel.SetMassScheme(HQms)
-apfel.SetAlphaQCDRef(alphaQCDref,QalphaQCDref) 
+apfel.SetAlphaQCDRef(alphaQCDref,QalphaQCDref)
 apfel.SetAlphaQEDRef(alphaQEDref,QalphaQEDref)
 apfel.SetPoleMasses(mcpole,mbpole,mtpole)
 apfel.EnableIntrinsicCharm(True)
@@ -88,10 +97,19 @@ apfel.SetMaxFlavourPDFs(nflmax)
 apfel.InitializeAPFEL()
 apfel.InitializeAPFEL_DIS()
 
+# Compute predictions
+for irep in range(1,nrep+1):
 
+#    lhapdf.usePDFMember(irep) This doesn't exist in lhapdf...
 
+    for iexp in range(0, nexp):
+        for iset in range(0,nset):
+            for ipt in range(1, int(npt[iexp][iset])+1):
 
+                if Q[ipt][iexp][iset] > 1e0:
 
+                    print("great")
 
-
-    
+                else:
+                    print("no")
+                    # thobs[irep][ipt][iexp][iset]  =  0e0
