332eb2868f787f9dc760e9be55740bd889392355
RosalynLP <rosalynlaurap@gmail.com>
2018-02-02 16:38:10 +0000

    Python observables code

--- Observables/theorypredict.py
+++ Observables/theorypredict.py
@@ -18,17 +18,9 @@ def list3D(a, b, c):
     lst = [[ ['#' for col in range(c)] for col in range(b)] for row in range(a)]
     return lst
 
-nexp     = 2
-nset     = 2
-maxpoint = 1000
-
-npt      = list2D(nexp,nset)
-obs      = list2D(nexp,nset)
-x        = list3D(maxpoint,nexp,nset)
-Q2       = list3D(maxpoint,nexp,nset)
-y        = list3D(maxpoint,nexp,nset)
-exobs    = list3D(maxpoint,nexp,nset)
-Q        = list3D(maxpoint,nexp,nset)
+def list4D(a, b, c, d):
+    lst = [[ [['#' for col in range(d)] for col in range(c)] for col in range(b)] for row in range(a)]
+    return lst
 
 # Initialise data files to be read
 exp      = ["CHORUS", "NTV"]
@@ -41,6 +33,19 @@ print("PDF set: " + pdfset)
 
 pdfs     =  lhapdf.mkPDFs(pdfset)
 nrep     =  len(pdfs)
+
+nexp     = 2
+nset     = 2
+maxpoint = 1000
+
+npt      = list2D(nexp,nset)
+obs      = list2D(nexp,nset)
+x        = list3D(maxpoint,nexp,nset)
+Q2       = list3D(maxpoint,nexp,nset)
+y        = list3D(maxpoint,nexp,nset)
+exobs    = list3D(maxpoint,nexp,nset)
+Q        = list3D(maxpoint,nexp,nset)
+thobs    = list4D(nrep,maxpoint,nexp,nset)
 
 # Reading data
 for iexp in range(0,nexp):
@@ -100,16 +105,45 @@ apfel.InitializeAPFEL_DIS()
 # Compute predictions
 for irep in range(1,nrep+1):
 
-#    lhapdf.usePDFMember(irep) This doesn't exist in lhapdf...
+    apfel.SetReplica(irep)
 
     for iexp in range(0, nexp):
         for iset in range(0,nset):
             for ipt in range(1, int(npt[iexp][iset])+1):
 
-                if Q[ipt][iexp][iset] > 1e0:
+                if float(Q[ipt][iexp][iset]) > 1e0:
 
-                    print("great")
+                    print("Q > 1")
+                    apfel.ComputeStructureFunctionsAPFEL(Q0,float(Q[ipt][iexp][iset]))
+                    print("Test 1")
+                    apfel.SetFKObservable(obs[iexp][iset])
+                    print("Test 2")
+
+                    thobs[irep][ipt][iexp][iset] = apfel.FKObservables(x[ipt][iexp][iset],
+                                                                        Q[ipt][iexp][iset],
+                                                                        y[ipt][iexp][iset])
+
+                    print("Theory observable:  " + str(thobs[irep][ipt][iexp][iset]))
 
                 else:
-                    print("no")
-                    # thobs[irep][ipt][iexp][iset]  =  0e0
+
+                     print("Q < 1, setting theory result to 0")
+                     thobs[irep][ipt][iexp][iset]  =  0e0
+
+# Writing results to file
+for iexp in range(0,nexp):
+    for iset in range(0,nset):
+
+    with open('/res/pyres/pOBS_{0}{1}.res'.format(exp[iexp],expset[iset]), 'w') as output:
+
+        output.write(str(obs[iexp][iset]) + "\t" + str(npt[iexp][iset]) + "\n")
+
+        for irep in range (1,nrep+1):
+
+            output.write(str(irep) + "\n")
+
+            for ipt in range(1,int(npt[iexp][iset])+1):
+
+                output.write(str(ipt) + "\t" + str(x[ipt][iexp][iset]
+                            + "\t" + str(thobs[irep][ipt][iexp][iset] + "\t"
+                            + str(exobs[ipt][iexp][iset] + "\n"))
