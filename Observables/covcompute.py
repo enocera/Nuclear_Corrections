7b917ececd6b88d591adb5fb315c16dfa29f1ddd
Rosalyn <rosalynlaurap@gmail.com>
2018-02-11 22:03:37 +0000

    Update code for computing cov

--- Observables/covcompute.py
+++ Observables/covcompute.py
@@ -4,30 +4,10 @@ import lhapdf
 import apfel
 import numpy as np
 import sys
-from math import sqrt
-
-def getelement(datafile,x,y):
-    element = (datafile.split("\n")[y]).split("\t")[x]
-    return element
-
-def list2D(a, b):
-    lst = [['#' for col in range(b)] for row in range(a)]
-    return lst
-
-def list3D(a, b, c):
-    lst = [[ ['#' for col in range(c)] for col in range(b)] for row in range(a)]
-    return lst
-
-def list4D(a, b, c, d):
-    lst = [[ [['#' for col in range(d)] for col in range(c)] for col in range(b)] for row in range(a)]
-    return lst
-
-def list5D(a, b, c, d, e):
-    lst = [[ [[ ['#' for col in range(e)] for col in range(d)] for col in range(c)] for col in range(b)] for row in range(a)]
-    return lst
+import matplotlib.pyplot as plt
 
 # Read in PDF set names
-nuclearpdf  =  raw_input("***** Please choose iron or lead: ")
+nuclearpdf  =  input("***** Please choose iron or lead: ")
 if nuclearpdf != "iron" and nuclearpdf != "lead":
     print("Error: Invalid choice of element")
     sys.exit()
@@ -37,61 +17,138 @@ print("----------------------------------------------------")
 print("Computing theory covariance matrix for " + nuclearpdf + " relative to proton PDF " + protonpdf)
 print("----------------------------------------------------")
 
-# nucpdfs     =  lhapdf.mkPDFs(nuclearpdf)
-# ppdfs       =  lhapdf.mkPDFs(protonpdf)
-# nnucrep     =  len(nucpdfs)
-# nprep       =  len(ppdfs)
-
-
 # Initialise data files to be read
 exp      = ["CHORUS", "NTV"]
 expset   = [["NU", "NB"], ["NUDMN", "NBDMN"]]
 
-if nuclearpdf = "iron":
+if nuclearpdf == "iron":
 
     nuclearpdfs = ["DSSZ_NLO_Fe56_MC_1000_compressed_250",
                    "EPPS16nlo_CT14nlo_Fe56_MC_1000_compressed_250",
                    "nCTEQ15FullNuc_56_26_MC_1000_compressed_250"]
 
-else if nuclearpdf = "lead":
+elif nuclearpdf == "lead":
 
     nuclearpdfs = ["DSSZ_NLO_Pb208_MC_1000_compressed_250",
                    "EPPS16nlo_CT14nlo_Pb208_MC_1000_compressed_250",
                    "nCTEQ15FullNuc_208_82_MC_1000_compressed_250"]
 
-nexp     = 2
-nset     = 2
-npdf     = 2
-maxpoint = 1000
+pdfs     = [protonpdf] + nuclearpdfs
 
-npt      = list2D(nexp,nset)
-thobs    = list5D(npdf,nrep+1,maxpoint,nexp,nset)
-datpoint =
-nrep     = ['#','#']
+nexp      = 2
+nset      = 2
+npdf      = len(pdfs)
+maxpoint  = 1000
+maxrep    = 251
 
-#
-#
-# # Reading data
-# for iexp in range(0,nexp):
-#     for iset in range(0,nset):
-#         for ipdf in range(0,npdf):
-#
-#             file = "res/pyres/pOBS_{0}{1}_{2}.res".format(exp[iexp],expset[iexp][iset],pdfset)
-#             print("-----------------------------------------------------------------")
-#             print("File being read is " + str(file))
-#             print("-----------------------------------------------------------------")
-#             with open(file) as contents:
-#                 contents = contents.read()
-#
-#             nrep[ipdf] = getelement(contents,2,0)
-#             print("Number of replicas for pdf " + str(pdfset[ipdf]) ": "+ str(npt[iexp][iset]))
-#
-#             npt[iexp][iset] = getelement(contents,1,0)
-#             print("Number of data points: " + str(npt[iexp][iset]))
-#
-#             for irep in range(0,int(nrep[ipdf])):
-#
-#                 datpoint[i]
-#                 thobs[ipt][iexp][iset] = getelement(contents,5,ipt)
-#
-#                 Q[ipt][iexp][iset]     = str(sqrt(float(Q2[ipt][iexp][iset])))
+nrep      = np.zeros(npdf,dtype=int)
+npt       = np.zeros((nexp,nset),dtype=int)
+thobs     = np.zeros((npdf,maxrep,maxpoint,nexp,nset))
+
+F_p       = np.zeros((maxpoint,nexp,nset))
+
+
+ # Read data
+for iexp in range(0,nexp):
+    for iset in range(0,nset):
+        for ipdf in range(0,npdf):
+
+             file = "res/pyres/pOBS_{0}{1}_{2}.res".format(exp[iexp],expset[iexp][iset],pdfs[ipdf])
+             print("-----------------------------------------------------------------")
+             print("File being read is " + str(file))
+             print("-----------------------------------------------------------------")
+             with open(file) as contents:
+                 contents = contents.read()
+                 
+             lines = contents.split('\n')
+             tabs  = [line.split('\t') for line in lines] 
+
+
+             nrep[ipdf] = int(tabs[0][2])
+             print("Number of replicas for pdf " + str(pdfs[ipdf]) + ": " + str(nrep[ipdf]))
+
+             npt[iexp,iset] = int(tabs[0][1])
+             print("Number of data points: " + str(npt[iexp,iset]))
+
+             for irep in range(0,nrep[ipdf]):
+                 datalines = [2 + irep*(1 + npt[iexp,iset]), 
+                             1 + (irep + 1)*(1 + npt[iexp,iset])]
+
+                 
+                 for fileline in range(datalines[0],datalines[1]):
+                     ipt                            = 1 + fileline - datalines[0]
+                     thobs[ipdf,irep,ipt,iexp,iset] = float(tabs[fileline][2])
+
+
+# Calculating central proton observable
+for iexp in range(0,nexp):
+    for iset in range(0,nset):
+        for ipt in range(1,npt[iexp][iset]+1):
+
+            F_p[ipt,iexp,iset] = (1/(nrep[0]))*np.sum(thobs[0,1:,ipt,iexp,iset])
+
+# Calculate theory covariance matrix
+#   1.  Combine nuclear PDF sets
+
+thobs_nuc = np.zeros(((len(nrep)-1)*maxrep,maxpoint,nexp,nset))
+
+for ipdf in range(1,npdf):
+
+    thobs_nuc[(ipdf-1)*maxrep:ipdf*maxrep] = thobs[ipdf]
+
+#   2.  Calculate total N_rep for combined set
+
+nrepnuc = np.sum(nrep) - nrep[0] - len(nuclearpdfs)
+print("Combined nuclear set replica number: " + str(nrepnuc))
+
+#   3.  Find covariance matrix
+
+s = np.zeros((maxpoint,maxpoint,nexp,nset))
+
+for iexp in range(0,nexp):
+    for iset in range(0,nset):
+
+        for i in range(0,npt[iexp,iset]):
+            for j in range(0,npt[iexp,iset]):
+                s[i,j,iexp,iset] = (1/nrepnuc)*np.sum(
+                                     (thobs_nuc[:,i,iexp,iset] - F_p[i,iexp,iset])*(
+                                      thobs_nuc[:,j,iexp,iset] - F_p[j,iexp,iset])) 
+
+#   4.  Normalise to central theory as percentage
+
+spct = np.zeros((maxpoint,maxpoint,nexp,nset))
+
+for iexp in range(0,nexp):
+    for iset in range(0,nset):
+
+        for i in range(0,npt[iexp,iset]):
+            for j in range(0,npt[iexp,iset]):
+
+                spct[i,j,iexp,iset] = 100*s[i,j,iexp,iset]/np.sqrt(thobs[0,0,i,iexp,iset]*thobs[0,0,j,iexp,iset])
+
+
+print(thobs[0,0,:,1,1])
+print(s[34,34,1,1])
+print(s[23,34,1,1])
+print(s[12,32,1,1])
+print(s[33,21,1,1])
+print(s[31,39,1,1])
+print(s[14,37,1,1])
+#   5.  Plot percentage covariance matrix
+
+for iexp in range(0,nexp):
+    for iset in range(0,nset):
+
+        fig=plt.figure()
+        ax1 = fig.add_subplot(111)
+        plt.gca().invert_yaxis()
+        cmap = ax1.pcolor(np.abs(spct[:int(npt[iexp,iset]),
+                                      :int(npt[iexp,iset]), iexp,iset]))
+        fig.colorbar(cmap, label = "% of theory")
+        plt.title("{0} {1} {2}".format(exp[iexp], expset[iexp][iset], nuclearpdf))
+        plt.show()
+
+
+
+
+
