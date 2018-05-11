f53c3505f95b9d31b5254ab487be065f56f7535d
Rosalyn <rosalynlaurap@gmail.com>
2018-05-11 15:39:39 +0100

    function to calculate Ks

--- strangeness_fraction/calc_Rs.py
+++ strangeness_fraction/calc_Rs.py
@@ -4,6 +4,7 @@ import lhapdf
 import numpy as np
 import sys
 from math import sqrt
+import scipy.integrate as integrate
 
 # Reading in command line arguments
 pdfset      = str(sys.argv[1])
@@ -17,10 +18,10 @@ pdfs        = lhapdf.mkPDFs(pdfset)
 N           = len(pdfs)
 
 # Creating  dictionaries for dynamic variable assignment
-dict        = {"f_3":np.zeros(N),
-               "f_-1":np.zeros(N), 
-               "f_-2":np.zeros(N), 
-               "f_-3":np.zeros(N)}
+dict        = {"f_3":np.zeros(N-1),
+               "f_-1":np.zeros(N-1), 
+               "f_-2":np.zeros(N-1), 
+               "f_-3":np.zeros(N-1)}
 
 errordict   = {"f_3":None,
                "f_-1":None, 
@@ -28,9 +29,10 @@ errordict   = {"f_3":None,
                "f_-3":None}
 
 # Loop over flavours and members to fill matrices with values
+# (ignoring 0th replica) 
 for pid in [-1,-2,3,-3]:
-    for k in range(N):
-        dict["f_{0}".format(pid)][k] = pdfs[k].xfxQ(pid, x, Q)
+    for k in range(0,N-1):
+        dict["f_{0}".format(pid)][k] = pdfs[k+1].xfxQ(pid, x, Q)
 
 # Calculating MC PDF errors
 for pid in [-1,-2,3,-3]:
@@ -51,13 +53,16 @@ delRs       = sqrt( (1/(ubar+dbar)**2)*(
               +  (1/(ubar+dbar)**2)*(
                      (errordict["f_-1"])**2 + (errordict["f_-2"])**2)))
 
+# Now to calculate K_s
+
+numerator    = integrate.quad(lambda t: (p.xfxQ(3, t, Q) + p.xfxQ(-3, t, Q)), 0, 1)
+denominator  = integrate.quad(lambda t: (p.xfxQ(-1, t, Q) + p.xfxQ(-2, t, Q)), 0, 1)
+
+Ks = numerator[0]/denominator[0]
 
 print("***********************************************************")
 print("PDF set {0}   ".format(pdfset))
 print("x = {0}, Q = {1} GeV".format(x,round(Q,3)))
-print("Rs:  ".format(pdfset) + str(round(Rs,4)) + " +/- " + str(round(delRs,4)))
+print("Rs:  " + str(round(Rs,4)) + " +/- " + str(round(delRs,4)))
+print("Ks:  " + str(round(Ks,4)))
 print("***********************************************************")
-
-
-
-
