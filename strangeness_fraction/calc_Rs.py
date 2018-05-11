6e41a5cbd6c3f9156ce5100bc131009aebd350b6
Rosalyn <rosalynlaurap@gmail.com>
2018-05-11 16:35:31 +0100

    Updating Ks calc

--- strangeness_fraction/calc_Rs.py
+++ strangeness_fraction/calc_Rs.py
@@ -6,7 +6,7 @@ import sys
 from math import sqrt
 import scipy.integrate as integrate
 
-# Reading in command line arguments
+# Reading in PDF set and setting x and Q values
 pdfset      = str(sys.argv[1])
 x           = 0.023
 Q           = sqrt(1.9)
@@ -18,21 +18,27 @@ pdfs        = lhapdf.mkPDFs(pdfset)
 N           = len(pdfs)
 
 # Creating  dictionaries for dynamic variable assignment
-dict        = {"f_3":np.zeros(N-1),
-               "f_-1":np.zeros(N-1), 
-               "f_-2":np.zeros(N-1), 
-               "f_-3":np.zeros(N-1)}
+dict        = {"f_3":np.zeros(N),
+               "f_-1":np.zeros(N), 
+               "f_-2":np.zeros(N), 
+               "f_-3":np.zeros(N)}
 
-errordict   = {"f_3":None,
+errordict     = {"f_3":None,
                "f_-1":None, 
                "f_-2":None, 
                "f_-3":None}
 
+R = np.zeros(N)
+
 # Loop over flavours and members to fill matrices with values
 # (ignoring 0th replica) 
-for pid in [-1,-2,3,-3]:
-    for k in range(0,N-1):
-        dict["f_{0}".format(pid)][k] = pdfs[k+1].xfxQ(pid, x, Q)
+for k in range(0,N):
+    for pid in [-1,-2,3,-3]:
+        dict["f_{0}".format(pid)][k] = pdfs[k].xfxQ(pid, x, Q)
+    R[k] = (dict["f_3"][k] + dict["f_-3"][k])/(dict["f_-2"][k] + dict["f_-1"][k])
+
+R_cent = R[0]
+R_err  = np.sqrt(np.mean(np.square(R[1:])) - np.square(np.mean(R[1:])))
 
 # Calculating MC PDF errors
 for pid in [-1,-2,3,-3]:
@@ -48,21 +54,29 @@ dbar        = p.xfxQ(-1,  x, Q)
 
 Rs          = (s + sbar)/(ubar + dbar)
 
+
+
 delRs       = sqrt( (1/(ubar+dbar)**2)*(
                      (errordict["f_3"])**2 + (errordict["f_-3"])**2
               +  (1/(ubar+dbar)**2)*(
-                     (errordict["f_-1"])**2 + (errordict["f_-2"])**2)))
+                  (errordict["f_-1"])**2 + (errordict["f_-2"])**2)))
 
-# Now to calculate K_s
+# Calculating Ks and error
+K = np.zeros(N)
+for k in range (0,N):
+    numerator    = integrate.quad(lambda t: (pdfs[k].xfxQ(3, t, Q) + p.xfxQ(-3, t, Q)), 0, 1)
+    denominator  = integrate.quad(lambda t: (pdfs[k].xfxQ(-1, t, Q) + p.xfxQ(-2, t, Q)), 0, 1)
 
-numerator    = integrate.quad(lambda t: (p.xfxQ(3, t, Q) + p.xfxQ(-3, t, Q)), 0, 1)
-denominator  = integrate.quad(lambda t: (p.xfxQ(-1, t, Q) + p.xfxQ(-2, t, Q)), 0, 1)
+    K[k] = numerator[0]/denominator[0]
 
-Ks = numerator[0]/denominator[0]
+K_cent = K[0]
+K_err = np.sqrt(np.mean(np.square(K[1:])) - np.square(np.mean(K[1:])))
 
 print("***********************************************************")
 print("PDF set {0}   ".format(pdfset))
 print("x = {0}, Q = {1} GeV".format(x,round(Q,3)))
 print("Rs:  " + str(round(Rs,4)) + " +/- " + str(round(delRs,4)))
-print("Ks:  " + str(round(Ks,4)))
+print("R_cent:  " + str(round(R_cent, 4)) + " +/- " + str(round(R_err,4)))
+#print("Ks:  " + str(round(Ks,4)))
+print("K_cent:  " + str(round(K_cent, 4)) + " +/- " + str(round(K_err,4)))
 print("***********************************************************")
--- strangeness_fraction/plot_Rs.py
+++ strangeness_fraction/plot_Rs.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 labels         = ["NNPDF3.0",
                   "NNPDF3.1",
                   "NNPDF3.1 calculated by me",
+                  "NNPDF3.1 debugged NUTEV",
+                  "NNPDF3.1 debugged NUTEV + nuclear corr",
                   "NNPDF3.1 collider-only",
                   "NNPDF3.1 HERA + ATLAS W,Z",
-                  "xFitter HERA + ATLAS W,Z",
-                  "NNPDF3.1 debugged NUTEV",
-                  "NNPDF3.1 debugged NUTEV + nuclear corr"]
+                  "xFitter HERA + ATLAS W,Z"]
 
-Rs              = [0.45, 0.59, 0.60, 0.82, 1.03, 1.13, 0.68, 0.70]
-Rserror         = [0.09, 0.12, 0.14,  0.18, 0.38, 0.11, 0.14, 0.15]
+Rs              = [0.45, 0.59, 0.60, 0.68, 0.70, 0.82, 1.03, 1.13]
+Rserror         = [0.09, 0.12, 0.13, 0.15, 0.16, 0.18, 0.38, 0.11]
 x               = [8, 7, 6, 5, 4, 3, 2, 1]
 
 plt.errorbar(Rs,x, xerr=Rserror, fmt="ro")
-
 plt.yticks(x, labels)
+plt.xlabel("$R_s$")
 plt.margins(0.2)
 plt.subplots_adjust(left=0.5)
 plt.show()
