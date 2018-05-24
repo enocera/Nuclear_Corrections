f607ce0108200991aa540703a7f12619494fa1c5
RosalynLP <rosalynlaurap@gmail.com>
2018-05-24 11:07:35 +0100

    Adding 68%CL calc

--- strangeness_fraction/calc_Rs.py
+++ strangeness_fraction/calc_Rs.py
@@ -38,7 +38,12 @@ for k in range(0,N):
     R[k] = (dict["f_3"][k] + dict["f_-3"][k])/(dict["f_-2"][k] + dict["f_-1"][k])
 
 R_cent = R[0]
+
+# 1 sigma errors:
 R_err  = np.sqrt(np.mean(np.square(R[1:])) - np.square(np.mean(R[1:])))
+
+# 68%CL errors:
+R_err2 = np.ptp(np.sort(R[1:])[16:84])
 
 # Calculating MC PDF errors
 for pid in [-1,-2,3,-3]:
@@ -46,37 +51,21 @@ for pid in [-1,-2,3,-3]:
         np.mean(np.square(dict["f_{0}".format(pid)]),axis=0)
         - np.square(np.mean(dict["f_{0}".format(pid)],axis=0)))
 
-# Calculating Rs and error
-s           = p.xfxQ(3,  x, Q)
-sbar        = p.xfxQ(-3, x, Q)
-ubar        = p.xfxQ(-2, x, Q)
-dbar        = p.xfxQ(-1,  x, Q)
-
-Rs          = (s + sbar)/(ubar + dbar)
-
-
-
-delRs       = sqrt( (1/(ubar+dbar)**2)*(
-                     (errordict["f_3"])**2 + (errordict["f_-3"])**2
-              +  (1/(ubar+dbar)**2)*(
-                  (errordict["f_-1"])**2 + (errordict["f_-2"])**2)))
-
 # Calculating Ks and error
 K = np.zeros(N)
 for k in range (0,N):
-    numerator    = integrate.quad(lambda t: (pdfs[k].xfxQ(3, t, Q) + p.xfxQ(-3, t, Q)), 0, 1)
-    denominator  = integrate.quad(lambda t: (pdfs[k].xfxQ(-1, t, Q) + p.xfxQ(-2, t, Q)), 0, 1)
+    numerator    = integrate.quad(lambda t: (pdfs[k].xfxQ(3, t, Q) + p.xfxQ(-3, t, Q)), 0.2, 0.8)
+    denominator  = integrate.quad(lambda t: (pdfs[k].xfxQ(-1, t, Q) + p.xfxQ(-2, t, Q)), 0.2, 0.8)
 
     K[k] = numerator[0]/denominator[0]
 
 K_cent = K[0]
 K_err = np.sqrt(np.mean(np.square(K[1:])) - np.square(np.mean(K[1:])))
+K_err2 = np.ptp(np.sort(K[1:])[16:84])
 
 print("***********************************************************")
 print("PDF set {0}   ".format(pdfset))
 print("x = {0}, Q = {1} GeV".format(x,round(Q,3)))
-print("Rs:  " + str(round(Rs,4)) + " +/- " + str(round(delRs,4)))
-print("R_cent:  " + str(round(R_cent, 4)) + " +/- " + str(round(R_err,4)))
-#print("Ks:  " + str(round(Ks,4)))
-print("K_cent:  " + str(round(K_cent, 4)) + " +/- " + str(round(K_err,4)))
+print("R_s:  " + str(round(R_cent, 4)) + " +/- " + "1 sigma: " + str(round(R_err,4)) + "  68%CL: " + str(round(R_err2,4)) )
+print("K_s:  " + str(round(K_cent, 4)) + " +/- "  + "1 sigma: " + str(round(K_err,4)) + "  68%CL: " + str(round(K_err2,4)))
 print("***********************************************************")
