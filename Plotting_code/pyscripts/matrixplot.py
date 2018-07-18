1d4bbcd6d0630ef935e66019281c8b7fe42e971c
Emanuele Nocera <noceral@tplxlap211.nat.physics.ox.ac.uk>
2018-07-18 22:27:02 +0100

    Added python script to produce paper plots

--- Plotting_code/pyscripts/matrixplot.py
+++ Plotting_code/pyscripts/matrixplot.py
@@ -15,22 +15,21 @@ dict = {}
 # Initialise data files to be read
 exp       = ["CHORUS", "NTV", "DYE605"]
 expset    = [["NU", "NB"], ["NUDMN", "NBDMN"], [""]]
-element   = ["lead", "iron", "copper"]
 npt       = [607, 45, 119]
 
 nexp      = 3
 nset      = [2, 2, 1]
 
-sigmacomb = [np.zeros((2*npt[0],2*npt[0])) , np.zeros((2*npt[1],2*npt[1])) , np.zeros((2*npt[2],2*npt[2]))]
-expthcomb = [np.zeros((2*npt[0],2*npt[0])) , np.zeros((2*npt[1],2*npt[1])) , np.zeros((2*npt[2],2*npt[2]))]
-thcomb    = [np.zeros((2*npt[0],2*npt[0])) , np.zeros((2*npt[1],2*npt[1])) , np.zeros((2*npt[2],2*npt[2]))]
+sigmaexp = [np.zeros((2*npt[0],2*npt[0])) , np.zeros((2*npt[1],2*npt[1])) , np.zeros((npt[2],npt[2]))]
+sigmatot = [np.zeros((2*npt[0],2*npt[0])) , np.zeros((2*npt[1],2*npt[1])) , np.zeros((npt[2],npt[2]))]
+sigmanuc    = [np.zeros((2*npt[0],2*npt[0])) , np.zeros((2*npt[1],2*npt[1])) , np.zeros((npt[2],npt[2]))]
 
 # Load experimental covariance matrix
 sigmadf   = [pd.read_table("../../Covariance/validphys/CHORUS/baseline/output/tables/experiments_covmat.csv", dtype={'user_id': float}) ,
              pd.read_table("../../Covariance/validphys/NTV/baseline/output/tables/experiments_covmat.csv", dtype={'user_id': float}) ,
              pd.read_table("../../Covariance/validphys/DYE605/baseline/output/tables/experiments_covmat.csv", dtype={'user_id': float})]
 
-sigmacomb = [sigmadf[0].iloc[3:,3:].values.astype(np.float) ,
+sigmaexp = [sigmadf[0].iloc[3:,3:].values.astype(np.float) ,
              sigmadf[1].iloc[3:,3:].values.astype(np.float) ,
              sigmadf[2].iloc[3:,3:].values.astype(np.float)]
 
@@ -39,19 +38,19 @@ expthdf   = [pd.read_table("../../Covariance/validphys/CHORUS/corr/output/tables
              pd.read_table("../../Covariance/validphys/NTV/corr/output/tables/experiments_covmat.csv", dtype={'user_id': float}) ,
              pd.read_table("../../Covariance/validphys/DYE605/corr/output/tables/experiments_covmat.csv", dtype={'user_id': float})]
 
-expthcomb = [expthdf[0].iloc[3:,3:].values.astype(np.float) ,
+sigmatot = [expthdf[0].iloc[3:,3:].values.astype(np.float) ,
              expthdf[1].iloc[3:,3:].values.astype(np.float) ,
              expthdf[2].iloc[3:,3:].values.astype(np.float)]
-    
+
 impactthresh       = [10,10,10]
-covthresh          = [1,0.1,1]
+covthresh          = [1,1,1]
 impactlim          = [10000,10000,10000]
 covlim             = [1000,1000,1000]
 
 for iexp in range(0,nexp):
     for iset in range(0,nset[iexp]):
         dict["exp_dat_{0}{1}".format(exp[iexp],expset[iexp][iset])] = np.loadtxt(
-            "../../Data/commondata/DATA_{0}{1}.dat".format(exp[iexp],expset[iexp][iset]), 
+            "../../Data/commondata/DATA_{0}{1}nucl.dat".format(exp[iexp],expset[iexp][iset]), 
             delimiter   = "\t",
             skiprows    = 1,
             usecols     = (5,)    
@@ -64,22 +63,22 @@ for iexp in range(0,nexp):
     else:
         expdat = dict["exp_dat_{0}{1}".format(exp[iexp],expset[iexp][0])]
 
-    thcomb[iexp] = expthcomb[iexp] - sigmacomb[iexp]
+    sigmanuc[iexp] = sigmatot[iexp] - sigmaexp[iexp]
     
-    diag_minus_half_exp   = (np.diagonal(sigmacomb[iexp]))**(-0.5)
-    corrmat_exp           =  diag_minus_half_exp*sigmacomb[iexp]*diag_minus_half_exp[:,np.newaxis]
+    diag_minus_half_exp   = (np.diagonal(sigmaexp[iexp]))**(-0.5)
+    corrmat_exp           =  diag_minus_half_exp*sigmaexp[iexp]*diag_minus_half_exp[:,np.newaxis]
     
-    diag_minus_half_nuc   = (np.diagonal(thcomb[iexp]))**(-0.5)
-    corrmat_nuc           =  diag_minus_half_nuc*thcomb[iexp]*diag_minus_half_nuc[:,np.newaxis]
+    diag_minus_half_nuc   = (np.diagonal(sigmanuc[iexp]))**(-0.5)
+    corrmat_nuc           =  diag_minus_half_nuc*sigmanuc[iexp]*diag_minus_half_nuc[:,np.newaxis]
     
-    diag_minus_half_tot   = (np.diagonal(expthcomb[iexp]))**(-0.5)
-    corrmat_tot           =  diag_minus_half_tot*expthcomb[iexp]*diag_minus_half_tot[:,np.newaxis]
+    diag_minus_half_tot   = (np.diagonal(sigmatot[iexp]))**(-0.5)
+    corrmat_tot           =  diag_minus_half_tot*sigmatot[iexp]*diag_minus_half_tot[:,np.newaxis]
     
     #####################################################################################
     
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    mat = ax1.matshow(100*thcomb[iexp]/np.outer(expdat,expdat),
+    mat = ax1.matshow(100*sigmanuc[iexp]/np.outer(expdat,expdat),
                       cmap=cm.Spectral_r,
                       norm=mcolors.SymLogNorm(linthresh=covthresh[iexp],
                                               linscale=10, vmin=-covlim[iexp],
@@ -92,13 +91,13 @@ for iexp in range(0,nexp):
     
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    mat = ax1.matshow(100*sigmacomb[iexp]/np.outer(expdat,expdat),
+    mat = ax1.matshow(100*sigmaexp[iexp]/np.outer(expdat,expdat),
                       cmap=cm.Spectral_r,
                       norm=mcolors.SymLogNorm(linthresh=covthresh[iexp], 
                                               linscale=10, vmin=-covlim[iexp], 
                                               vmax=covlim[iexp]))
     fig.colorbar(mat, label = "% of central theory")
-    plt.title("{0} experiment covariance matrix".format(exp[iexp]))
+    plt.title("{0} experimental covariance matrix".format(exp[iexp]))
     plt.savefig("../figs/covplot_exp_{0}".format(exp[iexp]))
     
     #####################################################################################
@@ -106,7 +105,7 @@ for iexp in range(0,nexp):
     #Plotting impact matrices
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    matrix = (expthcomb[iexp])/sigmacomb[iexp]
+    matrix = (sigmatot[iexp])/sigmaexp[iexp]
     mat = ax1.matshow(matrix, 
                       cmap=cm.Spectral_r,
                       norm=mcolors.SymLogNorm(linthresh=impactthresh[iexp],
@@ -127,7 +126,7 @@ for iexp in range(0,nexp):
                       vmin=-1, 
                       vmax=1)
     fig.colorbar(mat)
-    plt.title("{0} experiment correlation matrix".format(exp[iexp]))
+    plt.title("{0} experimental correlation matrix".format(exp[iexp]))
     plt.savefig("../figs/corrplot_exp_{0}".format(exp[iexp]))
     
     #-Nuc
@@ -159,15 +158,15 @@ for iexp in range(0,nexp):
     #####################################################################################
     
     fig = plt.figure()
-    plt.plot(np.sqrt(np.diag(sigmacomb[iexp]))/expdat,
+    plt.plot(np.sqrt(np.diag(sigmaexp[iexp]))/expdat,
              '.', 
              label="Experiment", 
              color="orange")
-    plt.plot(np.sqrt(np.diag(thcomb[iexp]))/expdat,
+    plt.plot(np.sqrt(np.diag(sigmanuc[iexp]))/expdat,
              '.', 
              label="Theory",
              color="darkorchid")
-    plt.plot(np.sqrt(np.diag(expthcomb[iexp]))/expdat,
+    plt.plot(np.sqrt(np.diag(sigmatot[iexp]))/expdat,
              '.', 
              label="Experiment + Theory", 
              color="teal")
@@ -182,11 +181,11 @@ for iexp in range(0,nexp):
     #####################################################################################
     
     fig = plt.figure()
-    plt.plot((np.diag(la.inv(sigmacomb[iexp])))**(-0.5)/expdat,
+    plt.plot((np.diag(la.inv(sigmaexp[iexp])))**(-0.5)/expdat,
              '.', 
              label="Experiment",
              color="orange")
-    plt.plot((np.diag(la.inv(expthcomb[iexp])))**(-0.5)/expdat,
+    plt.plot((np.diag(la.inv(sigmatot[iexp])))**(-0.5)/expdat,
              '.', 
              label="Experiment + Theory",
              color="mediumseagreen")
@@ -206,7 +205,6 @@ for iexp in range(0,nexp):
     plt.tight_layout()
     plt.savefig("../figs/observable_{0}".format(exp[iexp]))
 
-    continue
     
     
     
